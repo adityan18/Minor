672eb3893ff768dae1592ea8897bd4374b9e66b5
adityan18 <aditya.sushma2001@gmail.com>
2022-06-29 06:09:26 +0530

    pyChk

--- DataSet/binary_conversion.py
+++ DataSet/binary_conversion.py
@@ -1,12 +1,23 @@
-from numpy import dtype
+from traceback import print_tb
+from numpy import dtype, sign
 from fxpmath import Fxp
+import numpy as np
 
 
 x = 125.19856
 
+h = np.array([['0x999', '0x999'], ['0x666', '0x666']])
+
+z = Fxp(h, signed=True, dtype='S4.12')
+y = np.array(z)
+print(y)
+
+for i in z:
+    x = np.array(i)
+    print(type(x))
+# print(h.shape)
+
+
 y = Fxp(x, signed=True, dtype='S8.8')
-
+# print(y.info())
 print(y.bin(frac_dot=True))
-
-
-
